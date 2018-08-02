e652f3219b620f3acd90f781eefabaad4a1b679a
benfarice <imzoughene@outlook.com>
2018-08-02 16:21:11 +0100

    template | master page or Layout

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,18 @@ Route::get('/', function () {
 });
 
 
+Route::get('accueil', function () {
+    return view('accueil');
+});
+
+Route::get('contact', function () {
+    return view('contact');
+});
+
+Route::get('service', function () {
+    return view('service');
+});
+
 /*
 Route::get('contact', function () {
    echo "Je m'appele Youssef Imzoughene";
@@ -37,3 +49,7 @@ Route::get('listcontact','ContactController@listContacts');
 Route::get('profil/{name}/id/{id}', function ($name,$id) {
    echo "Je m'appele $name my id is $id";
 })->where(['name'=>'[a-zA-Z]+','id'=>'[0-9]+']);
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
